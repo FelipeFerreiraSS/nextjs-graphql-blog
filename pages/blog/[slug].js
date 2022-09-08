eda3ed16c7239f1796931b35fd726f18fcc6462f
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-09-08 19:57:58 -0300

    index and blog

---
+++ pages/blog/[slug].js
@@ -0,0 +1,68 @@
+import { GraphQLClient } from 'graphql-request';
+
+export default function Post({ post }) {
+  
+  return (
+    <div>
+      <img src={post.coverImage.url} />
+
+      <div>
+        <div>
+          <h1>
+            {post.title}
+          </h1>
+        </div>
+        
+        <div
+          dangerouslySetInnerHTML={{ __html: post.contentHtml.html }} 
+        />
+      </div>
+    </div>
+  )
+}
+
+const hygraph = new GraphQLClient(process.env.NEXT_PUBLIC_CONTENT_API);
+
+export async function getStaticProps({ params }) {
+  const { post } = await hygraph.request(
+    `
+    query Post($slug: String!) {
+      post(where: { slug: $slug }) {
+        title
+        contentHtml {
+          html
+        }
+        coverImage {
+          url(transformation: {image: {resize: {width: 900, height: 400, fit: crop}}})
+        }
+      }
+    }
+  `,
+    {
+      slug: params.slug,
+    }
+  );
+
+  return {
+    props: {
+      post,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const { posts } = await hygraph.request(`
+    {
+      posts {
+        slug
+      }
+    }
+  `);
+
+  return {
+    paths: posts.map(({ slug }) => ({
+      params: { slug },
+    })),
+    fallback: false,
+  };
+}
